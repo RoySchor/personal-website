61a0c51d816d8cdd4fc3333566e6a008c845ba0d
Roy Schor <rschor@justworks.com>
2025-12-19 23:08:22 -0500

    allowing debugging and local host on phones

--- 3d-personal-portfolio-website/vite.config.js
+++ 3d-personal-portfolio-website/vite.config.js
@@ -2,4 +2,14 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/personal-website/",
+  server: {
+    host: true,
+    proxy: {
+      "/personal-website/screen": {
+        target: "http://localhost:5174",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
