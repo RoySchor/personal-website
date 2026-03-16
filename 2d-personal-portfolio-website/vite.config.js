b27ada0208885dbb06c7a33cab4203788241d7ac
Roy Schor <rps5836@psu.edu>
2026-03-15 23:50:21 -0400

    changing base urls back to swithc from linode to squarespace provider

--- 2d-personal-portfolio-website/vite.config.js
+++ 2d-personal-portfolio-website/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/personal-website/screen/",
+  base: "/screen/",
   plugins: [react()],
 });
